8588c1e0c5daf6c0d438090f56cabae16f300dad
kisalu-id <152627403+kisalu-id@users.noreply.github.com>
2024-04-04 22:20:26 +0300

    Update Calculator.cs
    Edited the regex expression and indentation, left the rest of the code as I wrote it in Nov 2023

--- Calculator.cs
+++ Calculator.cs
@@ -4,64 +4,64 @@ using System.Linq;
 
 namespace Rechner
 {
-	class Program
-	{
-		static void Main(string[]args)
-		{
-		    try
-		    {
-		        Regex validateNumberRegex = new Regex("^\\d+$");
-		        
-		        Console.WriteLine("Enter first number and press Enter:");
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Regex validateNumberRegex = new Regex(@"^-?\d+(?:\.\d+)?(?:,\d+)?$");
+
+                Console.WriteLine("Enter first number and press Enter:");
                 string x1 = Console.ReadLine();
-                
-		        Console.WriteLine("Enter second number and press Enter:");
-                string x2 = Console.ReadLine();
-                if(
-                    (!validateNumberRegex.IsMatch(x1)) 
-                    || (!validateNumberRegex.IsMatch(x2))
-                 ) {
-                   // Console.WriteLine("Error");
-                }
-                
-                double _x1 = double.Parse(x1); 
-                double _x2 = double.Parse(x2);
-                    
+
                 Console.WriteLine("Enter operator and press Enter:");
                 string opt = Console.ReadLine();
+
+                Console.WriteLine("Enter second number and press Enter:");
+                string x2 = Console.ReadLine();
                 
+                if ((!validateNumberRegex.IsMatch(x1)) || (!validateNumberRegex.IsMatch(x2)))
+                {
+                    // Console.WriteLine("Error");
+                }
+
+                double _x1 = double.Parse(x1);
+                double _x2 = double.Parse(x2);
+
                 switch (opt)
                 {
                     case "*":
-                    Console.WriteLine(_x1 * _x2);
-                    break;
-            	    case "-":
-                    Console.WriteLine(_x1 - _x2);
-                    break;
+                        Console.WriteLine(_x1 * _x2);
+                        break;
+                    case "-":
+                        Console.WriteLine(_x1 - _x2);
+                        break;
                     case "+":
-                    Console.WriteLine(_x1 + _x2);
-                    break;
+                        Console.WriteLine(_x1 + _x2);
+                        break;
                     case "%":
-                    Console.WriteLine(String.Format("Result is {0}", _x1 % _x2));
-                    break;
+                        Console.WriteLine(String.Format("Result is {0}", _x1 % _x2));
+                        break;
                     case "/":
-                    if (_x2==0)
-                    {
-                        Console.WriteLine("Dividing on 0 is not possible");
-                    }
-                    else
-                    {
-                    Console.WriteLine(_x1 / _x2);
-                    }
-                    break;
+                        if (_x2 == 0)
+                        {
+                            Console.WriteLine("Dividing on 0 is not possible");
+                        }
+                        else
+                        {
+                            Console.WriteLine(_x1 / _x2);
+                        }
+                        break;
                     default:
-                    Console.WriteLine("Error");
-                    break;
+                        Console.WriteLine("Error");
+                        break;
                 }
-		    } catch
-                {
+            }
+            catch
+            {
                 Console.WriteLine("There's an error...");
-                }
-		}
-	}
+            }
+        }
+    }
 }
